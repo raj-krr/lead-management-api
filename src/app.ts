3c44388fe52091fc8956511e00d89bcf8d3541a1
raj-krr <mr.rajkumar3579@gmail.com>
2026-06-18 01:37:15 +0530

    refactor: reorganize middleware usage and improve code readability

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,31 @@ import express from "express";
 
 import leadRoutes from "./routes/lead.routes";
 import importRoutes from "./routes/import.routes";
-import { limiter } from "./middleware/rateLimit.middleware";
-const app = express();
 
+import { limiter } from "./middleware/rateLimit.middleware";
 import { apiKeyAuth } from "./middleware/apiKey.middleware";
 
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
+const app = express();
+
 const swaggerDocument =
   YAML.load("./swagger/swagger.yaml");
 
-  app.use(
+app.set("trust proxy", 1);
+
+app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument)
 );
-app.use(apiKeyAuth);
 
 app.use(express.json());
+
 app.use(limiter);
 
+app.use(apiKeyAuth);
 
 app.use("/api/leads", leadRoutes);
 
